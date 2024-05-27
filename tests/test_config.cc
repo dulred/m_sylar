4624e45545d0e3e95a33017c21d70306c44a48ad
dulred <dulred@qq.com>
2024-05-28 03:48:44 +0800

    配置系统  初步 stage 2 （引入yaml-cpp 文件库，读取和遍历yaml文件）

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -1,5 +1,6 @@
 #include "../sylar/config.h"
 #include "../sylar/log.h"
+#include <yaml-cpp/yaml.h>
 
 sylar::ConfigVar<int>::ptr g_int_value_confg = 
     sylar::Config::Lookup("system.port", (int)8080, "system port");
@@ -7,8 +8,45 @@ sylar::ConfigVar<int>::ptr g_int_value_confg =
 sylar::ConfigVar<float>::ptr g_float_value_confg = 
     sylar::Config::Lookup("system.value", (float)0.2f, "system value");
 
+
+void print_yaml(const YAML::Node& node, int level) {
+    if(node.IsScalar()) {
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                << node.Scalar() << " - " << node.Type() << " - " << level;
+    } else if(node.IsNull()) {
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                << "NULL - " << node.Type() << " - " << level;
+    } else if(node.IsMap()){
+        for(auto it = node.begin();
+                it != node.end(); ++it) {
+                     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                            << it->first << " - " << it->second.Type() << " - " << level;
+                     print_yaml(it->second, level + 1);
+                }
+    } else if (node.IsSequence())
+    {
+        for (size_t i = 0; i < node.size(); i++)
+        {
+            SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level * 4, ' ')
+                << i << " - "  << node[i].Type() << " - " << level;
+                print_yaml(node[i], level + 1);
+        }
+        
+    }
+    
+}
+
+void test_yaml() {
+    YAML::Node root = YAML::LoadFile("/home/dulred/sylar/bin/conf/log.yml");
+    print_yaml(root, 0);
+    // SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root;
+}
+
+
 int main (int argc, char** argv) {
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_confg->getValue();
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_float_value_confg->toString();
+
+    test_yaml();
     return 0;
 }
